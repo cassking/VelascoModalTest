bda3281e8234b285a08aaf58825b4e0882ea1504
Cassandra King <cassandra.ramirez.king@gmail.com>
2014-05-30 17:23:44 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$("div.modal").hide();
+
+$("span#open").click(function() {
+  $("div.modal").show("slow");
+  $("h1").fadeTo("slow", 0.33);
+  $("p").fadeTo("slow", 0.33);
+ });
+
+
+$("span#close").click(function() {
+  $("div.modal").hide("slow");
+  $("h1").fadeTo("slow", 1.0);
+  $("p").fadeTo("slow", 1.0);
+ });
